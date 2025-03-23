186197fccf4041d43cf14732ed65530ff74ee805
jihed-ben-salah <jihed.bensalah@esprit.tn>
2025-03-23 13:18:52 +0100

    langchain

--- src/data_processing/database_utils.py
+++
@@ -1,6 +0,0 @@
-import yaml
-
-with open("config/database.yaml", "r") as f:
-    config = yaml.safe_load(f)
-
-db_config = config["database"]
--- src/data_processing/text_extraction.py
+++ src/data_processing/text_extraction.py
@@ -2,30 +2,34 @@ from pdf2image import convert_from_path
 import pytesseract
 import os
 
-# Convert PDF to images
-def extract_text_from_pdf(pdf_path):
-    """
-    use OSC to extract text from a PDF file
-    :param pdf_path: path to the PDF file
-    """
-    images = convert_from_path(pdf_path)
-    extracted_text = ""
-    for image in images:
-        extracted_text += pytesseract.image_to_string(image)
-    return extracted_text
+class TextExtractor:
+    # Convert PDF to images
+    def __init__(self):
+        pass
+    
+    def extract_text_from_pdf(pdf_path):
+        """
+        use OSC to extract text from a PDF file
+        :param pdf_path: path to the PDF file
+        """
+        images = convert_from_path(pdf_path)
+        extracted_text = ""
+        for image in images:
+            extracted_text += pytesseract.image_to_string(image)
+        return extracted_text
 
 
-def process_dataset(dataset_path, output_path):
-    """
-    process a dataset of PDF files
-    :param dataset_path: path to the dataset
-    :param output_path: path to the output file
-    """
-    documents = os.listdir(dataset_path)
-    for doc in documents:
-        doc_path = os.path.join(dataset_path, doc)
-        text = extract_text_from_pdf(doc_path)
-        with open(output_path, "a") as f:
-            f.write(text)
-            f.write("\n")
+    def process_dataset(dataset_path, output_path):
+        """
+        process a dataset of PDF files
+        :param dataset_path: path to the dataset
+        :param output_path: path to the output file
+        """
+        documents = os.listdir(dataset_path)
+        for doc in documents:
+            doc_path = os.path.join(dataset_path, doc)
+            text = extract_text_from_pdf(doc_path)
+            with open(output_path, "a") as f:
+                f.write(text)
+                f.write("\n")
     
